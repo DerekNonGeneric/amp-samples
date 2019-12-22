0982c0155c814bff96ec802b9bb23a3056c3161e
Derek Lewis <DerekNonGeneric@inf.is>
2019-12-22 14:30:13 -0500

    build: fix stale cryption

--- tasks/config.js
+++ tasks/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branchName: 'amp-sort', // Must be changed to your sample's remote branch name
   destDir: 'dist',
-  host: 'dereklewis.github.io',  // Must be changed to your username or organization name
+  host: 'DerekNonGeneric.github.io',  // Must be changed to your username or organization name
   repoName: 'amp-samples',
 };
